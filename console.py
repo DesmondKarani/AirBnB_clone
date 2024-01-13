7542a5d625dcef17f94b908dcf488191bd7e97a5
Desmond <karanidesmond@protonmail.ch>
2024-01-13 15:26:33 +0300

    update

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ HBNBCommand module: Entry point of the command interpreter
 
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,9 +21,36 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """Method called when an empty line is entered in
-        response to the prompt."""
+        """Method called when an empty line is entered in response
+        to the prompt."""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it and prints the id"""
+        if not arg:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Shows the string representation of an instance"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            if key in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
